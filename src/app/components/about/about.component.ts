1a4262da1cde052daa121f638041cbb0c4fd9b3d
Eduardo Branco <75887699+dEvBranco@users.noreply.github.com>
2021-03-29 15:13:43 +0100

    Final update

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { PokemonInfo } from 'src/app/models/pokemon.interface';
 
 @Component({
   selector: 'app-about',
@@ -8,10 +11,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class AboutComponent implements OnInit {
-  constructor(private router: ActivatedRoute) { }
-  public routeParam: string;
+  constructor(private router: ActivatedRoute, private http: HttpClient) {}
+  private routeId: string;
+  public pokemonObs: Observable<PokemonInfo>;
+  ngOnInit(): void {
+    this.routeId = this.router.snapshot.params.id;
+    this.initPokemonData();
+  }
 
-  ngOnInit() {
-    this.routeParam = this.router.snapshot.params.id;
+  private initPokemonData(): void {
+    this.pokemonObs = this.http.get<PokemonInfo>(
+      `https://pokeapi.co/api/v2/pokemon/${this.routeId}`
+    );
   }
 }
